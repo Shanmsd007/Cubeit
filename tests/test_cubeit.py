89631cc562aa682c7ef4e0f62228aea444e257e1
Shanmugam <shanpsg146@gmail.com>
2025-04-21 15:31:28 +0530

    Tests in git

--- tests/test_cubeit.py
+++ tests/test_cubeit.py
@@ -1,2 +1,12 @@
 from playwright.sync_api import *
-import pytest
+
+def test_cube(page:Page):
+    num = "10"
+    page.goto("http://127.0.0.1:8000/")
+    number = page.get_by_placeholder("Enter a number")
+    number.fill(num)
+    cta_btn = page.get_by_role("button", name="Calculate Cube")
+    cta_btn.click()
+    result = page.locator("p#result")
+    expect(result).to_have_text("The cube of 10 is: 1000 ")
+    page.close()
